773da8f036ac784fc5fee40b3dfe06ea4c33a672
Ahmed Fouad Ali <ahmmad.f@hotmail.com>
2023-07-25 02:04:25 +0300

    end of lesson 1

---
+++ lesson_1.dart
@@ -0,0 +1,28 @@
+void main() {
+  print("coding with fun");
+  //variable declaraion =⇒ variable type  variable name
+  //variable init ⇒ = "value" string literal which is our value ;
+  // ; terminator
+  // 1
+  // String myName = "Ahmed Fouad";
+  // int age = 21;
+  // double myHeigh = 1.6;
+  // bool isAdmin = true; //false
+
+  // print("My name is ${myName} and my age ${age} \$");
+  // print('I\'am');
+
+  int a = 200;
+  print('$a');
+  print('\$a');
+  print('\$$a');
+  String message = """
+  My name is :
+
+  asdasd
+
+
+
+ """;
+  print(message);
+}
