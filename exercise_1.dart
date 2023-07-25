c71eb38f72d6a675578755b8df883222d5cf6ba0
Ahmed Fouad Ali <ahmmad.f@hotmail.com>
2023-07-25 04:36:39 +0300

    end of lesson 4

---
+++ exercise_1.dart
@@ -0,0 +1,8 @@
+// Exercise: Temperature conversion
+// double tempFarenheit = 90;
+// vert the temperature to Celsius, using this formula:
+// Fahrenheit to Celsius:   (°F − 32) / 1.8 = °C
+// Then, print the result. The output of this program should be:
+// the converted temperature in celsius should show at most 1 fractional digit
+//90F = 32.2C
+
---
+++ lesson_4.dart
@@ -0,0 +1,46 @@
+void main() {
+  // print(5 + 3);
+  // print(5 - 3);
+  // print(5 / 3);
+  // print(5 * 3);
+  // print(5 ~/ 3);
+  // print(5 % 3);
+  // print(5 % 2);
+  // print(158 % 10);
+  // () */% +-
+  // int x = 10;
+  // x = x + 5;
+  // x *= 5;
+  // x ~/= 5;
+  // print(x);
+  // int y = 20;
+  // y = y + 1;
+  // y += 1;
+  // y++;
+  // ++y;
+  // int w = ++y;
+  // print(y);
+  // print(w);
+
+  // int x = 1;
+  // int y = x++; //1
+  // int z = --y; // 0 , 0
+  // print('x: $x, y: $y, z: $z');
+  bool isCorrect = 5 == 3;
+  isCorrect = 5 != 3;
+  isCorrect = 5 <= 3;
+  isCorrect = 5 < 3;
+  isCorrect = 5 >= 3;
+  isCorrect = 5 > 3;
+  // && both must be true to be true;
+  // || one must be true to be true;
+//   int age = 15;
+// //              condition ? is true take first : if false take second
+//   String type = age >= 19 ? "adult" : "child";
+//   print(type);
+
+//   String email = "";
+//   int x = 3;
+//   print(email.isNotEmpty || x++ > 5);
+//   print(x);
+}
